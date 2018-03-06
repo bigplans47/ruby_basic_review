6d0f3326a14b5b25a7474b7a661e0d0194301ac6
Dan Kiss <flowfast47@gmail.com>
2018-03-06 14:57:17 -0800

    add new class with instance of objects and reader methods

--- hello.rb
+++ hello.rb
@@ -4,5 +4,40 @@
 
 # puts 'Hello World, type your name'
 # name = gets.chomp
-name = ARGV[0]
-puts "hello #{name}"
+# class Word
+#   def initialize(word)
+#     puts "word initialized #{word}"
+#   end
+#   def scramble
+#     reverse().upcase()
+#   end
+# end
+#
+# word2 = Word.new("hello")
+class Cat
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+
+  def scramble
+    @name.reverse()
+  end
+
+end
+
+cat1 = Cat.new("kitty",6)
+cat2 = Cat.new('cattytat',5)
+
+puts cat1.age()
+puts cat2.scramble()
